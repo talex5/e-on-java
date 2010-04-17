310974505d9171f0f7c87eb1ffb904e350289e89
Thomas Leonard <talex5@gmail.com>
2010-04-17 16:50:24 +0100

    Change Loop to work directly on Callables
    Otherwise, E has to wrap each EImpl in a Java proxy.

--- src/jsrc/org/erights/e/elang/interp/Loop.java
+++ src/jsrc/org/erights/e/elang/interp/Loop.java
@@ -3,6 +3,10 @@ package org.erights.e.elang.interp;
 // Copyright 2002 Combex, Inc. under the terms of the MIT X license
 // found at http://www.opensource.org/licenses/mit-license.html ...............
 
+import org.erights.e.elib.base.Script;
+import org.erights.e.elib.base.Callable;
+import org.erights.e.elib.prim.E;
+
 
 /**
  * An StaticMaker on this class is the function named "__loop" in the
@@ -21,6 +25,7 @@ public class Loop {
      *
      */
     static public final Loop THE_ONE = new Loop();
+    static private final Object[] NO_ARGS = new Object[0];
 
     /**
      *
@@ -31,8 +36,15 @@ public class Loop {
     /**
      * Keep calling loopBody until it returns false.
      */
-    public void run(LoopBody loopBody) {
-        while (loopBody.run()) {
+    public void run(Callable loopBody) {
+        Script script = loopBody.optShorten("run", 0);
+        if (script == null) {
+            while (E.asBoolean(loopBody.callAll("run", NO_ARGS))) {
+            }
+        } else {
+            script = script.shorten(loopBody, "run", 0);
+            while (((Boolean) script.execute(loopBody, "run", NO_ARGS)).booleanValue()) {
+            }
         }
     }
 
